47580de63d0766947351cae1cd621c7a1eb5cfcb
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-03 19:54:24 -0300

    add tooltip

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -12,3 +14,4 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();

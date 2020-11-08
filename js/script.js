3d8fa800654953a189c6168c99e9e61019f14c3e
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-08 18:09:47 -0300

    add menu mobile

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/animacao-scoll.js';
-
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
 
 
 initScrollSuave();
@@ -15,3 +15,4 @@ initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();

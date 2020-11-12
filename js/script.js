c936a127996bfde98908f8da449d4f37b1697bf4
thiagonmiziara <thiagonmiziara@gmail.com>
2020-11-11 22:06:15 -0300

    add fetch animais

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -19,3 +20,4 @@ initDropdownMenu();
 initMenuMobile();
 initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
